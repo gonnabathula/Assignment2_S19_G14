9f48b32a1188e7e26363a05dbb8a99058be0b892
fdesm <fdesmie@gmail.com>
2019-02-10 18:05:05 -0500

    rebuilt

--- Assignment2_S19/Program.cs
+++ Assignment2_S19/Program.cs
@@ -27,8 +27,8 @@ namespace Assignment2_S19
 
             // Missing numbers
             Console.WriteLine("\n\nMissing numbers");
-            int[] arr1 = { 203, 204, 205, 206, 207, 208, 203, 204, 205, 206};
-            int[] brr = {203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204};
+            int[] arr1 = { 203, 204, 205, 206, 207, 208, 203, 204, 205, 206 };
+            int[] brr = { 203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204 };
             int[] r2 = missingNumbers(arr1, brr);
             displayArray(r2);
 
@@ -40,7 +40,7 @@ namespace Assignment2_S19
 
             // find the median
             Console.WriteLine("\n\nFind the median");
-            int[] arr2 = { 0, 1, 2, 4, 6, 5, 3};
+            int[] arr2 = { 0, 1, 2, 4, 6, 5, 3 };
             Console.WriteLine(findMedian(arr2));
 
             // closest numbers
@@ -53,11 +53,14 @@ namespace Assignment2_S19
             Console.WriteLine("\n\nDay of Programmer");
             int year = 2017;
             Console.WriteLine(dayOfProgrammer(year));
+            Console.ReadKey();
         }
 
-        static void displayArray(int []arr) {
+        static void displayArray(int[] arr)
+        {
             Console.WriteLine();
-            foreach(int n in arr) {
+            foreach (int n in arr)
+            {
                 Console.Write(n + " ");
             }
         }
@@ -65,7 +68,19 @@ namespace Assignment2_S19
         // Complete the rotLeft function below.
         static int[] rotLeft(int[] a, int d)
         {
-            return new int[] {};
+            int count = a.Length % d;
+            while (count <= d)
+            {
+                int temp = a[0];                      //assigning the first element in a temporary variable
+                for (int i = 0; i < a.Length - 1; i++) //shift the elements to left by 1
+                {
+                    a[i] = a[i + 1];
+                }
+                a[a.Length - 1] = temp;              // storing the element in temp array 
+                count = count + 1;
+            }
+            return a;
+            return new int[] { };
         }
 
         // Complete the maximumToys function below.
@@ -111,7 +126,7 @@ namespace Assignment2_S19
                     }
                 }
             }
-            //Console.ReadKey();
+            Console.ReadKey();
 
             return 0;
         }
@@ -125,6 +140,31 @@ namespace Assignment2_S19
         // Complete the missingNumbers function below.
         static int[] missingNumbers(int[] arr, int[] brr)
         {
+            int n1 = arr.Length;
+            int n2 = brr.Length;
+            int[] freq = new int[50];               //created a frequency array
+            freq[0] = 1;
+
+            int firstele = brr[0];                //storing the value of the first element from the orriginal array
+            for (int i = 1; i < n2; i++)         //computing the frequency of each element from original array in accordance with the first element
+            {
+                int diff = brr[i] - firstele;
+                freq[0 + diff]++;               //incrementing the frequency
+            }
+
+            for (int i = 0; i < n1; i++)       //computing the frequency of each element from array with missing values in accordance with the first element
+            {
+
+                int diff = arr[i] - firstele;
+                freq[0 + diff]--;             //decrementing the frequency
+            }
+
+            for (int i = 0; i < freq.Length; i++)  //iterate the frequency array
+            {
+                if (freq[i] > 0)
+                    Console.Write("{0} ", firstele + (i - 0)); //print the frequency array by computing using first element from the original array
+            }
+
             return new int[] { };
         }
 
@@ -138,7 +178,7 @@ namespace Assignment2_S19
         // Complete the findMedian function below.
         static int findMedian(int[] arr)
         {
-            Console.WriteLine("Sorted Array in Ascending Order");
+            //Console.WriteLine("Sorted Array in Ascending Order");
             int l = arr.Length; //lenght of the array.
             int minpos = 0;
             // selection sort of the given array.
@@ -156,7 +196,7 @@ namespace Assignment2_S19
                 }
 
 
-                Console.Write("  " + arr[i]); // to print the sorted array in ascending order.
+              //  Console.Write("  " + arr[i]); // to print the sorted array in ascending order.
             }
             {
 
@@ -170,12 +210,148 @@ namespace Assignment2_S19
         // Complete the closestNumbers function below.
         static int[] closestNumbers(int[] arr)
         {
+            arr = sortedArray(arr);                           //call the sorted array method
+            var minDiff = new List<int> { arr[0], arr[1] };  //creating list of numbers with minimum difference
+            var diff = absValue(arr[1], arr[0]);            //call absolute method to find the absolute values of first two elements
+
+
+            for (var i = 1; i < arr.Length - 1; i++)       //iterate array to find the difference between current and next elements and compare with the previous
+            {
+                var diff02 = absValue(arr[i], arr[i + 1]);
+                if (diff02 < diff)                        // if true assign the current and next elements to the list
+                {
+                    diff = diff02;
+                    minDiff = new List<int>() { arr[i], arr[i + 1] };
+                }
+                else if (diff02 == diff)                // if true add the current and next elements to the list
+                {
+                    minDiff.Add(arr[i]);
+                    minDiff.Add(arr[i + 1]);
+                }
+            }
+            return minDiff.ToArray();
             return new int[] { };
+        }
+
+        static int[] sortedArray(int[] arr)
+        {
+            int a = 0;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (a = 0; a < arr.Length - i - 1; a++) //check if next value is smaller than the current value, if next value is less, perform swap
+                {
+
+                    if (arr[a] > arr[a + 1])       //perform swap     
+                    {
+                        int temp = arr[a];
+                        arr[a] = arr[a + 1];
+                        arr[a + 1] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+
+
+        static int absValue(int a, int b) //method to compute the absolute values
+        {
+            var absDiff = 0;
+            if ((a <= 0 && b <= 0) || (a >= 0 && b >= 0))
+                absDiff = Math.Abs(Math.Abs(a) - Math.Abs(b));
+            return absDiff;
+
+
+            //return new int[] { };
         }
 
         // Complete the dayOfProgrammer function below.
         static string dayOfProgrammer(int year)
         {
+            string date = "";
+            if (year >= 1919)       // year greater than or equal to 1919 call the method to computeGregorianDays
+            {
+                date = computeGregorianDays(year);
+            }
+            else
+                if (year <= 1917)   // year less than or equal to 1917 call the method to computeJulianDays
+            {
+                date = computeJulianDays(year);
+            }
+            else               // year equal to 1918 call the method to compute1918
+            {
+                date = compute1918(year);
+            }
+            return date;
+            return "";
+        }
+
+        static bool isJulianLeap(int year) //Following the Julian Calender check leap year or not
+        {
+            if (year % 4 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        static bool isGregorianLeap(int year) //Following the Gregorian Calender check leap year or not
+        {
+            if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static string computeGregorianDays(int year) //method to compute days as per Gregorian calendar
+        {
+            int noOfDays = 0;
+            int programmerDateInSeptember = 0;
+            int countDaysTillAug_notLeap = 243; // till august since we need to find 256th day
+            int countDaysTillAug_Leap = 244;   // till august since we need to find 256th day
+            DateTime date;
+            string date01;
+            if (isGregorianLeap(year))
+            {
+                programmerDateInSeptember = 256 - countDaysTillAug_Leap;
+            }
+            else
+            {
+                programmerDateInSeptember = 256 - countDaysTillAug_notLeap;
+            }
+            date01 = programmerDateInSeptember.ToString() + ".09." + year.ToString();
+            return date01;
+
+        }
+        static string computeJulianDays(int year)   //method to compute days as per Julian calendar
+        {
+            int noOfDays = 0;
+            int programmerDateInSeptember = 0;
+            int countDaysTillAug_notLeap = 243; // till august since we need to find 256th day
+            int countDaysTillAug_Leap = 244;   // till august since we need to find 256th day
+            DateTime date;
+            string date02;
+            if (isJulianLeap(year))
+            {
+                programmerDateInSeptember = 256 - countDaysTillAug_Leap;
+            }
+            else
+            {
+                programmerDateInSeptember = 256 - countDaysTillAug_notLeap;
+            }
+            date02 = programmerDateInSeptember.ToString() + ".09." + year.ToString();
+            return date02;
+
+        }
+        static string compute1918(int year)      //method to compute days if the year was 1918 that is the year of transition
+        {
+            string date02;
+            int programmerDateInSeptember = 0;
+            int countDaysTillAug_notLeap = 230; // till august since we need to find 256th day
+            //int countDaysTillAug_Leap = 231;
+            programmerDateInSeptember = 256 - countDaysTillAug_notLeap;
+            date02 = programmerDateInSeptember.ToString() + ".09." + year.ToString(); //computing directly since 1918 is not a leap year
+            return "";
+
             Console.ReadKey();
             return "";
         }
